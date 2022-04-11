2917bd531968fab73f6d85eaff0e66dda37cf9b1
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2022-04-11 18:58:42 +0200

    add functionnal debug funcs

--- srcs/debug/debug_utils.c
+++ srcs/debug/debug_utils.c
@@ -6,14 +6,11 @@
 /*   By: cjulienn <cjulienn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 15:41:38 by cjulienn          #+#    #+#             */
-/*   Updated: 2022/04/10 14:39:39 by cjulienn         ###   ########.fr       */
+/*   Updated: 2022/04/11 18:57:47 by cjulienn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
-
-# define BROKEN 	0
-# define CORRECT	1
 
 /* debugging function : NOT TO PUSH FOR CORRECTION */
 
@@ -37,25 +34,24 @@ void	msg_accept_input(void)
 
 static void	mistake_report(t_data *data, int pile_id)
 {
-	int			*nums;
+	int			*nums_dup;
+	int			*pile;
 	int			i;
-	t_stack		*pile;
 
 	if (pile_id == ALPHA)
 		pile = data->pile_a;
 	else
 		pile = data->pile_b;
-	nums = pile_to_int_arr(pile);
-	if (!nums)
-		ft_putstr_fd("malloc error\n", STDERR_FILENO);
-	quicksort(nums, 0, data->stack_size - 1);
+	nums_dup = int_arr_dup(pile, data->stack_size);
+	if (!nums_dup)
+		ft_putstr_fd("malloc error !!!!\n", STDERR_FILENO);
+	quicksort(nums_dup, 0, data->stack_size - 1);
 	i = 0;
 	while (i < data->stack_size)
 	{
-		if (nums[i] != pile->num)
-			printf("diff at index %i : is %i and should be %i\n", i, pile->num, nums[i]);
+		if (nums_dup[i] != pile[i])
+			printf("diff at index %i : is %i and should be %i\n", i, pile[i], nums_dup[i]);
 		i++;
-		pile = pile->next;
 	}
 }
 
@@ -63,7 +59,7 @@ static void	mistake_report(t_data *data, int pile_id)
 
 void	is_stack_sorted(t_data *data, int pile_id)
 {
-	t_stack		*pile;
+	int			*pile;
 	int			*nums;
 	int			*nums_dup;
 	int			stack_size;
@@ -72,13 +68,10 @@ void	is_stack_sorted(t_data *data, int pile_id)
 		pile = data->pile_a;
 	else
 		pile = data->pile_b;
-	nums = pile_to_int_arr(pile);
-	if (! nums)
-		ft_putstr_fd("malloc error\n", STDERR_FILENO);
 	stack_size = data->stack_size;
 	nums_dup = int_arr_dup(nums, stack_size);
 	if (!nums_dup)
-		ft_putstr_fd("malloc error\n", STDERR_FILENO);
+		ft_putstr_fd("malloc error !!!!\n", STDERR_FILENO);
 	quicksort(nums_dup, 0, stack_size - 1);
 	if (are_arr_equals(nums, nums_dup, stack_size) == 1)
 		ft_putstr_fd("stack is sorted !!!!\n", STDOUT_FILENO);
@@ -93,29 +86,27 @@ void	is_stack_sorted(t_data *data, int pile_id)
 
 void	display_pile(t_data *data)
 {
-	t_stack		*iter;
-
+	int		i;
+	
 	if (data->pile_a)
 	{
 		ft_printf("PILE A\n");
-		iter = data->pile_a;
-		while (iter->next)
+		i = 0;
+		while (i < data->size_a)
 		{
-			ft_printf("%i\n", iter->num);
-			iter = iter->next;
+			printf("%i\n", data->pile_a[i]);
+			i++;
 		}
-		ft_printf("%d\n", iter->num);
 	}
 	if (data->pile_b)
 	{
-		printf("PILE B\n");
-		iter = data->pile_b;
-		while (iter->next)
+		ft_printf("PILE A\n");
+		i = 0;
+		while (i < data->size_b)
 		{
-			ft_printf("%d\n", iter->num);
-			iter = iter->next;
+			printf("%i\n", data->pile_b[i]);
+			i++;
 		}
-		ft_printf("%d\n", iter->num);
 	}
 	ft_printf("\n----------------------------------\n");
 }
